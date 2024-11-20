b71f7cd5b7fee78824f95d03fffbedac1a348c9b
FrierenDv <hackerxct007@gmail.com>
2024-11-21 05:44:15 +0700

    chore: add configuration file for bot settings and database connection

---
+++ config.js
@@ -0,0 +1,10 @@
+export default {
+	prefix: ["!", "/"],
+	owner: ["628385818401"],
+	bot_name: "Frieren",
+	bot_number: "628385818401",
+
+	database: {
+		mongo_url: "mongodb://localhost:27017/frieren",
+	},
+};
