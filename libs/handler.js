1947c4cb6574f3a48ccdf89d4bdac7e4f39910d4
FrierenDv <hackerxct007@gmail.com>
2024-11-21 21:55:57 +0700

    feat: refactor handler to improve command parsing and permission extraction

--- libs/handler.js
+++ libs/handler.js
@@ -1,43 +1,24 @@
 import config from "../config.js";
-import { logger } from "../shared/logger.js";
+import { logger, printer } from "../shared/logger.js";
 import wrap from "../shared/wrap.js";
 import db from "./database.js";
+import { handleFeatureError } from "./handler/error.js";
+import * as handlerParse from "./handler/parse.js";
 import queue from "./queue.js";
 
 /**
  * @param {import("surya").IClientSocket} client
- * @param {import("@frierendv/frieren").Api.Client} api
  * @param {import("@frierendv/frieren").Baileys.IParsedMessage} msg
+ * @param {import("@frierendv/frieren").Api.Client} api
  * @param {import("./feature-loader").default["features"]} features
  */
-export default async function handler(client, api, msg, features) {
-	const { sender, reply, text: params, isGroup } = msg;
+export default async function handler(client, msg, api, features) {
 	const { sock, store } = client;
 
-	const groupMetadata = isGroup ? await sock.groupMetadata(msg.from) : null;
+	const { groupMetadata, isOwner, isAdmin, isBotAdmin } =
+		await handlerParse.extractPermission(msg, sock, config);
 
-	wrap(() => sock.readMessages([msg.message.key]), logger.error);
-
-	const isOwner = (
-		Array.isArray(config.owners) ? config.owners : [config.owners]
-	)
-		.map((n) => n.replace(/[^\d]/g, "") + "@s.whatsapp.net")
-		.includes(sender);
-
-	const isAdmin =
-		isGroup && groupMetadata
-			? groupMetadata.participants
-					.filter((p) => p.admin)
-					.map((p) => p.id)
-					.includes(sender)
-			: false;
-	const isBotAdmin =
-		isGroup && groupMetadata
-			? groupMetadata.participants
-					.filter((p) => p.admin)
-					.map((p) => p.id)
-					.includes(sock.user?.id ?? "")
-			: false;
+	const { sender, reply, text: params, isGroup } = msg;
 
 	const user = db.users.set(sender);
 	user.name = msg.name;
@@ -50,128 +31,138 @@ export default async function handler(client, api, msg, features) {
 		user.premium_expired = 0;
 	}
 
-	let executed_feature;
-	try {
-		for (const feature of Object.values(features)) {
-			const isCommand = [
-				...config.prefix,
-				...(feature.customPrefix ?? []),
-			].some((p) => params?.startsWith(p));
-			const text = isCommand
-				? params?.slice(1 + params.slice(1).split(" ")[0].length + 1)
-				: params;
+	const settings = db.settings.set(sock.user.id);
+	if (shouldSkipMessage(settings, isGroup)) {
+		logger.info("Skipping message");
+		return;
+	}
 
-			const args = text?.split(" ");
-			const prefix =
-				config.prefix.find((p) => params?.startsWith(p)) ?? "";
-
-			const command = isCommand
-				? params?.split(" ")[0].slice(prefix.length)
-				: null;
-
-			const extras = {
-				text,
-				args,
-				prefix,
-				command,
-				isGroup,
-				isAdmin,
-				isOwner,
-				isBotAdmin,
-				groupMetadata,
-				api,
-				sock,
-				store,
-				db,
-				features,
-			};
-
-			if (feature.before && typeof feature.before === "function") {
-				await wrap(
-					() => feature.before(msg, extras),
-					(error) =>
-						handleFeatureError(feature, command, error, reply),
-					() => logger.info("Before command executed")
-				);
-			}
-
-			if (
-				isCommand &&
-				[...feature.command, ...(feature.customPrefix ?? [])].includes(
-					command
-				)
-			) {
-				if (queue.exist(sender, feature)) {
-					reply("You are still using this command");
-					continue;
-				}
-				if (feature.owner && !isOwner) {
-					reply("Only the owner can use this command.");
-					continue;
-				}
-				if (feature.admin && isGroup && !isAdmin) {
-					reply("Only the admin can use this command.");
-					continue;
-				}
-				if (feature.group && !isGroup) {
-					reply("This command only available in group");
-					continue;
-				}
-				if (feature.private && isGroup) {
-					reply("This command only available in private chat");
-					continue;
-				}
-				if (feature.limit && !isOwner && !user.premium) {
-					if (user.limit < 0) {
-						reply(
-							"You have reached the limit of using this command"
-						);
-						continue;
-					}
-				}
-
-				executed_feature = feature;
-				queue.add(sender, executed_feature);
-				await wrap(
-					() => feature.execute(msg, extras),
-					(error) =>
-						handleFeatureError(feature, command, error, reply),
-					() => {
-						if (feature.limit) {
-							user.limit--;
-						}
-					}
-				);
-			}
-
-			if (feature.after && typeof feature.after === "function") {
-				await wrap(
-					() => feature.after(msg, extras),
-					(error) =>
-						handleFeatureError(feature, command, error, reply),
-					() => logger.info("After command executed")
-				);
-			}
+	if (isGroup && groupMetadata) {
+		const group = db.groups.set(msg.from);
+		if (group.banned && !isOwner) {
+			return;
 		}
-	} catch (error) {
-		logger.error(error);
+		group.name = groupMetadata.subject;
+	}
+
+	// read message
+	wrap(() => sock.readMessages([msg.message.key]), logger.error);
+
+	let executed_feature;
+	for (const feature of Object.values(features)) {
+		const isCommand = [
+			...config.prefix,
+			...(feature.customPrefix ?? []),
+		].some((p) => params?.startsWith(p));
+
+		const _prefix = Array.isArray(config.prefix)
+			? config.prefix
+			: [config.prefix];
+
+		const { command, text, args, prefix } = handlerParse.extractCommand(
+			isCommand,
+			params,
+			_prefix
+		);
+		const extras = {
+			text,
+			args,
+			prefix,
+			command,
+			isGroup,
+			isAdmin,
+			isOwner,
+			isBotAdmin,
+			groupMetadata,
+			api,
+			sock,
+			store,
+			db,
+			features,
+			feature,
+		};
+
+		if (feature.before && typeof feature.before === "function") {
+			await wrap(
+				() => feature.before(msg, extras),
+				(error) => handleFeatureError(feature, command, error, reply),
+				() => {}
+			);
+		}
+
+		const shouldExecute =
+			isCommand &&
+			[...feature.command, ...(feature.customPrefix ?? [])].includes(
+				command
+			);
+
+		if (shouldExecute) {
+			if (queue.exist(sender, feature)) {
+				reply("You are still using this command");
+				continue;
+			}
+			if (feature.owner && !isOwner) {
+				reply("Only the owner can use this command.");
+				continue;
+			}
+
+			const isAdminCommand = feature.admin && isGroup && !isAdmin;
+			if (isAdminCommand) {
+				reply("Only the admin can use this command.");
+				continue;
+			}
+			if (feature.group && !isGroup) {
+				reply("This command only available in group");
+				continue;
+			}
+			if (feature.private && isGroup) {
+				reply("This command only available in private chat");
+				continue;
+			}
+
+			const shouldApplyLimit = feature.limit && !isOwner && !user.premium;
+			if (shouldApplyLimit) {
+				if (user.limit < 0) {
+					reply("You have reached the limit of using this command");
+					continue;
+				}
+			}
+
+			executed_feature = feature;
+			queue.add(sender, executed_feature);
+			await wrap(
+				() => feature.execute(msg, extras),
+				(error) => handleFeatureError(feature, command, error, reply),
+				() => {
+					if (feature.limit) {
+						user.limit--;
+					}
+				}
+			);
+		}
+
+		if (feature.after && typeof feature.after === "function") {
+			await wrap(
+				() => feature.after(msg, extras),
+				(error) => handleFeatureError(feature, command, error, reply),
+				() => {}
+			);
+		}
 	}
 	queue.remove(sender, executed_feature);
+
+	wrap(() => printer(msg, groupMetadata), logger.error);
 }
 
 /**
- * @param {{ failed: string; }} feature
- * @param {string | null | undefined} command
- * @param {unknown} error
- * @param {(text: string) => void} reply
+ * @param {Record<string, any>} settings
+ * @param {boolean} isGroup
+ * @returns {boolean}
  */
-function handleFeatureError(feature, command, error, reply) {
-	if (feature.failed) {
-		reply(
-			feature.failed
-				.replace("%cmd", command ?? "")
-				.replace("%error", String(error))
-		);
-	}
-	logger.error(error);
+function shouldSkipMessage(settings, isGroup) {
+	return (
+		(settings.self && isGroup) ||
+		(settings.groupOnly && !isGroup) ||
+		(settings.privateChatOnly && isGroup)
+	);
 }
