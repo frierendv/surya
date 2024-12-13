16d06e4ea2e664d80146f3bbc78029e0fd284526
FrierenDv <hackerxct007@gmail.com>
2024-12-13 18:45:57 +0700

    feat(rembg): add command to remove background from images

---
+++ features/rembg.js
@@ -0,0 +1,59 @@
+import uploader from "../libs/uploader.js";
+
+/**
+ * @type {import("surya").Feature}
+ */
+export default {
+	command: ["rembg", "removebg"],
+	description: "Remove background from image",
+	category: "Image",
+	owner: false,
+	admin: false,
+	hidden: false,
+	limit: false,
+	group: false,
+	private: false,
+
+	execute: async function (ctx, { api, prefix }) {
+		const media = ctx.quoted?.media ?? ctx.media;
+		if (!media || !/image/i.test(media.mimetype)) {
+			return ctx.reply(
+				`Reply/send image with *${prefix + this.command}*`
+			);
+		}
+		const buffer = await media.download();
+		const init_image = await uploader.providers.tmpfiles.upload(buffer);
+
+		const { data, error } = await api.post("/image/rembg", {
+			body: {
+				init_image,
+			},
+		});
+
+		if (error) {
+			return ctx.reply(
+				`Failed to execute the ${this.command} command\n${error.message}`
+			);
+		}
+		const { status, result, message } = data;
+		if (!status || !result?.images) {
+			return ctx.reply(message);
+		}
+
+		await ctx.sock.sendMessage(
+			ctx.from,
+			{
+				document: {
+					url: result.images[0],
+				},
+				fileName: `rembg-${ctx.from}.png`,
+				mimetype: "image/png",
+			},
+			{ quoted: ctx.message }
+		);
+	},
+
+	failed: "Failed to execute the %cmd command\n%error",
+	wait: null,
+	done: null,
+};
