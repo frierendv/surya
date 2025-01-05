7f5d46092e6d0bb3da30ed8ebb85d42fd67862ed
FrierenDv <hackerxct007@gmail.com>
2025-01-05 17:47:55 +0700

    feat(voice-changer): add voice change command for audio processing

---
+++ features/voice-changer.js
@@ -0,0 +1,78 @@
+/**
+ * @type {import("surya").Feature}
+ */
+export default {
+	command: ["vc", "voice-change"],
+	description: "Change voice from audio",
+	category: "Audio",
+	owner: false,
+	admin: false,
+	hidden: false,
+	limit: false,
+	group: false,
+	private: false,
+
+	execute: async function (ctx, { api, text, prefix }) {
+		const media = ctx.quoted?.media ?? ctx.media;
+		if (!media || !/audio/i.test(media.mimetype)) {
+			return ctx.reply(
+				`Reply/send audio with *${prefix + this.command}*`
+			);
+		}
+		const options = {
+			voice_id: "EXAVITQu4vr4xnSDxMaL",
+		};
+		const regex = /--(\w+)\s+([\w-]+)/g;
+		let match;
+		while ((match = regex.exec(text))) {
+			const [, key, value] = match;
+			options[key] = value;
+		}
+
+		const buffer = await media.download();
+
+		const form = new FormData();
+		const blob = new Blob([buffer], { type: media.mimetype });
+		form.append("audio", blob, "audio.mp3");
+		for (const key in options) {
+			form.append(key, options[key]);
+		}
+
+		const { data, error } = await api.post(
+			"/tts/inference_voice",
+			{
+				body: form,
+			},
+			{
+				headers: {
+					"Content-Type": "multipart/form-data",
+				},
+			}
+		);
+
+		if (error) {
+			return ctx.reply(
+				`Failed to execute the ${this.command} command\n${error.message}`
+			);
+		}
+		const { status, result, message } = data;
+		if (!status || !result?.audios) {
+			return ctx.reply(message);
+		}
+
+		await ctx.sock.sendMessage(
+			ctx.from,
+			{
+				audio: {
+					url: result.audios[0],
+				},
+				mimetype: "audio/mp4",
+			},
+			{ quoted: ctx.message }
+		);
+	},
+
+	failed: "Failed to execute the %cmd command\n%error",
+	wait: null,
+	done: null,
+};
