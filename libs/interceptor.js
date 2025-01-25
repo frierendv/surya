249922ed0c8c7a7805657c8de19e7421b45af3ff
FrierenDv <hackerxct007@gmail.com>
2025-01-25 18:20:44 +0700

    feat(interceptor): enhance translation function with timeout handling and fallback options

--- libs/interceptor.js
+++ libs/interceptor.js
@@ -1,5 +1,6 @@
 // @ts-nocheck
 import { translate as TranslateApi } from "@vitalets/google-translate-api";
+import { fetch } from "undici";
 
 /**
  *
@@ -36,21 +37,74 @@ export function translator(ctx) {
 }
 
 async function translate(text, target_lang) {
-	if (!target_lang) {
+	// wtf
+	if (!target_lang || !text) {
+		return text ?? "";
+	}
+	try {
+		const controller = new AbortController();
+		const timer = setTimeout(() => {
+			controller.abort();
+			// console.debug("[timeout] I win");
+			throw new Error("Translation timeout");
+		}, 5000);
+
+		const cleanUp = (result) => {
+			clearTimeout(timer);
+			controller.abort();
+			return result;
+		};
+		// race between two this sh(t)
+		const v1 = translateV1(text, target_lang, controller.signal).then(
+			cleanUp
+		);
+		const v2 = translateV2(text, target_lang, controller.signal).then(
+			cleanUp
+		);
+		const result = await Promise.race([v1, v2]);
+		return result ?? text;
+	} catch (error) {
+		if (error.name !== "AbortError") {
+			console.error("Translation error:", error);
+		}
 		return text;
 	}
-	// const timeout = new Promise((_, reject) =>
-	// 	setTimeout(() => reject(new Error("Translation timeout")), 3000)
-	// );
-	const translation = TranslateApi(text, {
-		to: target_lang.toLowerCase(),
-		fetchOptions: {
-			timeout: 10000,
-		},
-	}).catch(() => ({ text }));
-	const { text: translatedText } = await Promise.race([
-		translation,
-		// timeout,
-	]).catch(() => ({ text }));
-	return translatedText;
 }
+
+async function translateV1(text, target_lang, signal) {
+	const translation = await TranslateApi(text, {
+		to: target_lang,
+		fetchOptions: {
+			signal,
+		},
+	});
+	// console.debug("[v1] I win");
+	return translation.text;
+}
+
+async function translateV2(text, target_lang, signal) {
+	const requestBody = {
+		sl: "auto",
+		tl: target_lang,
+		q: text,
+	};
+	const response = await fetch(
+		"https://translate.googleapis.com/translate_a/single?client=gtx&dt=t&dj=1",
+		{
+			method: "POST",
+			body: new URLSearchParams(requestBody).toString(),
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded",
+			},
+			signal,
+		}
+	);
+	const { sentences } = await response.json();
+	// console.debug("[v2] I win");
+	return sentences
+		.filter((sentence) => "trans" in sentence)
+		.map((sentence) => sentence.trans)
+		.join("");
+}
+
+translate("Hello world", "ID").then(console.log);
