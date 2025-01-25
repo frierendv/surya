d31f04598b05c206a3e80554cd46920a70fbc84d
FrierenDv <hackerxct007@gmail.com>
2025-01-25 17:28:37 +0700

    feat(interceptor): add translation middleware functionality to message handling

---
+++ libs/interceptor.js
@@ -0,0 +1,50 @@
+// @ts-nocheck
+import { translate as TranslateApi } from "@vitalets/google-translate-api";
+
+/**
+ *
+ * @param {import("@frierendv/frieren").Baileys.IContextMessage} ctx
+ * @returns {import("@frierendv/frieren").Baileys.IContextMessage}
+ */
+export function translator(ctx) {
+	const { country } = ctx;
+
+	ctx.reply = async (text, opts) => ctx.reply(text, opts);
+	ctx.sock.sendMessage = async (jid, content, opts) => {
+		if (content?.text) {
+			content.text = await translate(content.text, country);
+		} else if (content?.caption) {
+			content.caption = await translate(content.caption, country);
+		}
+		return ctx.sock.sendMessage(jid, content, opts);
+	};
+	ctx.sock.sendFile = async (jid, file, filename, opts) => {
+		if (opts?.caption) {
+			opts.caption = await translate(opts.caption, country);
+		}
+		return ctx.sock.sendFile(jid, file, filename, opts);
+	};
+	return ctx;
+}
+
+async function translate(text, target_lang) {
+	if (!target_lang) {
+		return text;
+	}
+	// const timeout = new Promise((_, reject) =>
+	// 	setTimeout(() => reject(new Error("Translation timeout")), 3000)
+	// );
+	const translation = TranslateApi(text, {
+		to: target_lang,
+		fetchOptions: {
+			timeout: 3000,
+		},
+	}).catch(() => ({
+		text,
+	}));
+	const { text: translatedText } = await Promise.race([
+		translation,
+		// timeout,
+	]).catch(() => ({ text }));
+	return translatedText;
+}
