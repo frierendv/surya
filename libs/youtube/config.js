b32eb668cc31f15c08e4b62b0e69f5614a56101e
FrierenDv <hackerxct007@gmail.com>
2025-01-30 05:35:42 +0700

    refactor(youtube): remove obsolete Y2Mate class and configuration

--- libs/youtube/config.js
+++
@@ -1,9 +0,0 @@
-export const CONFIG = {
-	QUALITY: ["144p", "240p", "360p", "480p", "720p", "auto"],
-	BASE_URL: "https://www.y2mate.com",
-	DEFAULT_HEADERS: {
-		referer: "https://www.y2mate.com/",
-		"user-agent":
-			"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0",
-	},
-};
