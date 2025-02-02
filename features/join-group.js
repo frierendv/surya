2460f2d8ee047bb4962dda4acd0b8493c2186742
FrierenDv <hackerxct007@gmail.com>
2025-02-02 22:45:56 +0700

    feat(join-group): implement command to join a group using an invite link

---
+++ features/join-group.js
@@ -0,0 +1,60 @@
+/**
+ * @type {import("surya").Feature}
+ */
+export default {
+	command: ["join", "join-group"],
+	description: "Join a group using an invite link.",
+	category: "Group",
+	owner: false,
+	group: false,
+	admin: false,
+	hidden: false,
+	limit: false,
+	private: false,
+
+	execute: async function (ctx, { args }) {
+		const [groupInviteCode] =
+			args[0].match(/(https:\/\/chat.whatsapp.com\/[a-zA-Z0-9]+)/) || [];
+		if (!groupInviteCode) {
+			return ctx.reply(
+				"The provided group link is invalid. Please ensure you paste the entire link."
+			);
+		}
+		const groupInfo = await ctx.sock
+			.groupGetInviteInfo(groupInviteCode)
+			.catch(() => null);
+
+		if (!groupInfo) {
+			return ctx.reply(
+				"Unable to retrieve group information. Please verify that the link is correct."
+			);
+		}
+
+		const { size, participants, subject, isCommunity, joinApprovalMode } =
+			groupInfo;
+		if (isCommunity) {
+			return ctx.reply(
+				"Community groups are not supported. Only regular group chats are allowed."
+			);
+		}
+
+		const groupSize = size || participants.length;
+		if (groupSize <= 20) {
+			return ctx.reply(
+				"The group must have more than 20 members to join."
+			);
+		}
+
+		await ctx.sock.groupAcceptInvite(groupInviteCode);
+
+		return ctx.reply(
+			`Successfully joined the group: ${subject}. ${
+				(joinApprovalMode && "*Admin approval is required*") || ""
+			}`
+		);
+	},
+
+	failed: "Failed to execute the %cmd command\n%error",
+	wait: null,
+	done: null,
+};
