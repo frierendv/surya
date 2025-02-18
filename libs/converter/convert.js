b8f9f45e9ec08949d36d5a0438110a05a0c8daaa
FrierenDv <hackerxct007@gmail.com>
2025-02-18 18:24:26 +0700

    refactor(converter): rename format parameter to toFormat in convert function

--- libs/converter/convert.js
+++ libs/converter/convert.js
@@ -2,14 +2,14 @@ import { Ffmpeg } from "./ffmpeg.js";
 
 /**
  * @param {Buffer | import("stream").Readable} input
- * @param {string} format
+ * @param {string} toFormat
  * @param {string[]} outputOptions
  * @returns {Promise<Buffer>}
  * @throws {Error}
  */
-export const convert = async (input, format, outputOptions) => {
+export const convert = async (input, toFormat, outputOptions) => {
 	const ffmpeg = Ffmpeg(input, {
-		toFormat: format,
+		toFormat,
 		addOutputOptions: outputOptions,
 	});
 	const buffer = await ffmpeg.exec();
