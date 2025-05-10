a8f0fdbbc9bb5131541b8530be338befecc855e7
FrierenDv <itsrose.dev@gmail.com>
2025-05-11 01:37:48 +0700

    feat(converter): add convertAudio function for OGG format conversion

--- libs/converter/convert.js
+++ libs/converter/convert.js
@@ -15,3 +15,20 @@ export const convert = async (input, toFormat, outputOptions) => {
 	const buffer = await ffmpeg.exec();
 	return buffer;
 };
+
+/**
+ * Converts input audio to OGG format using FFmpeg.
+ *
+ * Ref: https://github.com/WhiskeySockets/Baileys/tree/feature/communities?tab=readme-ov-file#audio-message
+ * @param {Buffer} input - The input audio data.
+ * @returns {Promise<Buffer>} - The converted audio as a Buffer.
+ */
+export const convertAudio = async (input) => {
+	const outputOptions = ["-c:a", "libvorbis", "-b:a", "128k", "-ac", "1"];
+	const ffmpeg = Ffmpeg(input, {
+		outputOptions,
+		toFormat: "ogg",
+	});
+	const buffer = await ffmpeg.exec();
+	return buffer;
+};
