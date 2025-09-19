ee506fa1295426077ca15795d5d9e03b262d4455
FrierenDv <hackerxct007@gmail.com>
2025-09-20 02:21:51 +0700

    fix(tsup): remove splitting option and external dependency for baileys

--- packages/mongodb-auth-state/tsup.config.ts
+++ packages/mongodb-auth-state/tsup.config.ts
@@ -2,12 +2,9 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
 	entry: ["src/index.ts"],
-	splitting: true,
 	treeshake: true,
 	clean: true,
 	dts: true,
 	target: "es2020",
 	format: ["cjs", "esm"],
-
-	external: ["baileys"],
 });
