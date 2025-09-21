017f2ca8f4e41ad23670fa05547618d30ea71b69
FrierenDv <hackerxct007@gmail.com>
2025-09-22 06:27:40 +0700

    chore: update Jest configuration for coverage settings and test environment

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,17 @@ import type { Config } from "jest";
 
 const config = {
 	projects: ["<rootDir>/packages/*/jest.config.ts"],
+	coverageProvider: "v8",
+	coverageDirectory: "<rootDir>/coverage",
+	coverageThreshold: {
+		global: {
+			branches: 80,
+			functions: 80,
+			lines: 80,
+			statements: 80,
+		},
+	},
+	testEnvironment: "node",
 } satisfies Config;
 
 export default config;
