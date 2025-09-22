ba0ff03cd0d2be43ec05582e15e9eb062dc61afc
FrierenDv <hackerxct007@gmail.com>
2025-09-22 21:52:10 +0700

    feat: Refactor plugin manager to use logger and streamline plugin event handling

--- surya-rb/src/index.ts
+++ surya-rb/src/index.ts
@@ -1,22 +1,25 @@
+import { messageHandler } from "@handler/message";
+import { pluginHandler } from "@handler/plugin";
+import { logger } from "@libs/logger";
 import mongoose, { connectToDatabase } from "@libs/mongodb";
 import pm from "@libs/plugin-manager";
-import {
-	BaileysSocket,
-	createExtraMessageContext,
-	createMessageContext,
-} from "@surya/baileys-utils";
+import { BaileysSocket } from "@surya/baileys-utils";
+import { attachSendFile } from "@surya/baileys-utils/internals/send-file";
 import { useMongoDBAuthState } from "@surya/mongodb-auth-state";
-import P from "pino";
+import {
+	Browsers,
+	isJidBroadcast,
+	isJidMetaAI,
+	isJidNewsletter,
+	isJidStatusBroadcast,
+	jidNormalizedUser,
+} from "baileys";
 import QRCode from "qrcode";
 
-const prefix = [".", "!"];
-const logger = P({ level: "silent" });
-
 connectToDatabase().catch((err) => {
-	console.error("Failed to connect to MongoDB:", err);
+	logger.error({ err }, "Failed to connect to database");
 	process.exit(1);
 });
-pm.loadAll().then(() => pm.watch());
 
 const baileys = new BaileysSocket({
 	authProvider: useMongoDBAuthState({
@@ -26,51 +29,106 @@ const baileys = new BaileysSocket({
 	}),
 	socketConfig: {
 		logger,
+		shouldIgnoreJid: (jid) => {
+			return (
+				isJidBroadcast(jid) ||
+				isJidStatusBroadcast(jid) ||
+				isJidMetaAI(jid) ||
+				isJidNewsletter(jid)
+			);
+		},
+		browser: Browsers.ubuntu("Edge"),
 	},
 	maxReconnectAttempts: 10,
 	initialReconnectDelayMs: 3000,
 });
 
+let pairingRequested = false;
+/**
+ * If `SR_BOT_NUMBER` is set, request pairing code instead of printing QR code.
+ * Delete `auth` related collection in MongoDB to re-trigger pairing request.
+ * or encounter error, adjust the `Browser` in `socketConfig`.
+ */
+const requestPairing = async (qr: string) => {
+	if (!baileys.socket || baileys.socket.authState.creds.registered) {
+		return;
+	}
+	if (process.env.SR_BOT_NUMBER && !pairingRequested) {
+		const phoneNumber = process.env.SR_BOT_NUMBER.replace(/[^0-9]/g, "");
+		const code = await baileys.socket!.requestPairingCode(phoneNumber);
+		console.log(`Pairing code: ${code}.`);
+		pairingRequested = true;
+		return;
+	}
+	QRCode.toString(qr, { type: "terminal", small: true }, (err, url) => {
+		if (err) {
+			console.error("Failed to generate QR code:", err);
+		} else {
+			console.log(url);
+		}
+	});
+};
+
 baileys.on("connection.update", (update) => {
 	const { qr } = update;
 	if (qr) {
-		QRCode.toString(qr, { type: "terminal", small: true }, (err, url) => {
-			if (err) {
-				console.error("Failed to generate QR code:", err);
-			} else {
-				console.log(url);
-			}
-		});
+		requestPairing(qr).catch((err) =>
+			console.error("Failed to request pairing code:", err)
+		);
 	}
+
+	if (update.connection === "open") {
+		if (!baileys.socket?.user?.phoneNumber) {
+			const botId = jidNormalizedUser(baileys.socket?.user?.id);
+			Object.defineProperty(baileys.socket?.user, "phoneNumber", {
+				value: botId,
+				enumerable: true,
+			});
+			logger.warn(
+				{ id: baileys.socket?.user?.id, phoneNumber: botId },
+				"Assigning phoneNumber to bot user from ID."
+			);
+		}
+		logger.success(
+			{
+				name: baileys.socket?.user?.name,
+				id: baileys.socket?.user?.phoneNumber,
+			},
+			"Baileys connected"
+		);
+	}
+});
+baileys.on("lid-mapping.update", (update) => {
+	logger.info({ update }, "LID mapping updated");
 });
 
 baileys.on("messages.upsert", async (upsert) => {
-	if (upsert.type === "notify") {
-		for (const msg of upsert.messages) {
-			const ctx = createMessageContext(msg, baileys.socket!);
-			const extra = await createExtraMessageContext(ctx, baileys.socket!);
-
-			if (!extra.command || !prefix.includes(extra.prefix)) {
-				return;
-			}
-			const matches = pm.findByCommand(extra.command);
-			if (matches.length === 0) {
-				return;
-			}
+	if (upsert.type === "notify" && upsert.messages[0]) {
+		const result = await messageHandler(
+			upsert.messages[0],
+			baileys.socket!
+		);
+		if (result) {
+			const { matches, ctx, extra } = result;
+			// execute all matched plugins
 			await Promise.all(
-				matches.map((plugin) => pm.handle(plugin, ctx, extra))
+				matches.map((plugin) => pluginHandler(plugin, ctx, extra))
 			);
 		}
 	}
 });
 
-baileys.launch().catch((err) => {
-	console.error("Failed to launch Baileys socket:", err);
-	process.exit(1);
-});
+pm.loadAll().then(() => pm.watch());
+baileys
+	.launch()
+	.then(attachSendFile)
+	.catch((err) => {
+		logger.error({ err }, "Failed to launch Baileys socket");
+		process.exit(1);
+	});
 
 process.on("SIGINT", async () => {
-	console.log("Caught interrupt signal, shutting down...");
+	logger.fatal("Caught interrupt signal, shutting down...");
 	await baileys.stop();
 	process.exit(0);
 });
