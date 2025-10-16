7d9284af5411a553e96bedacdbb02cf55ff62535
FrierenDv <hackerxct007@gmail.com>
2025-10-17 03:29:44 +0700

    feat: add esbuild-fix-imports-plugin to devDependencies and configure in tsup

--- surya-rb/tsup.config.ts
+++ surya-rb/tsup.config.ts
@@ -1,10 +1,13 @@
+import { fixImportsPlugin } from "esbuild-fix-imports-plugin";
 import { defineConfig } from "tsup";
 
 export default defineConfig({
 	entry: ["src", "!src/**/*.md"],
 	splitting: false,
+	bundle: false,
 	clean: true,
 	target: "es2020",
 	format: ["esm"],
 	external: ["fluent-ffmpeg", "pino"],
+	esbuildPlugins: [fixImportsPlugin()],
 });
