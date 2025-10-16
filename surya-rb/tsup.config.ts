c837819e827bede9a053185ad375f3565126daf3
FrierenDv <hackerxct007@gmail.com>
2025-10-17 03:22:16 +0700

    refactor: update build script and tsup configuration for improved output

--- surya-rb/tsup.config.ts
+++ surya-rb/tsup.config.ts
@@ -1,12 +1,8 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-	entry: {
-		index: "src/index.ts",
-		plugins: "src/plugins",
-	},
-	splitting: true,
-	treeshake: true,
+	entry: ["src", "!src/**/*.md"],
+	splitting: false,
 	clean: true,
 	target: "es2020",
 	format: ["esm"],
